1dcfce2b36498c54b1b426ab83f7813c5f2443c2
Lance Hartung <hartung@cs.wisc.edu>
2026-08-12 11:42:58 -0500

    Implement repetitions of the consistency check

--- scripts/post-vulcan-review.js
+++ scripts/post-vulcan-review.js
@@ -1,35 +1,61 @@
 import fs from 'fs';
 
-
 const vulcan_project_url = 'https://github.com/VulcanToolkit';
+const num_repetitions = 5;
+const super_majority_threshold = 4;
+
+
+// Template for the consistency check header line.
+// Example: "concerning (4 / 5)"
+const consistencyHeader = (response, type) => `${response[type].result_string} (${response[type].votes} / ${response[type].total_votes})`;
+
+// Templates for the reason section.
+const reasonConclusive = (response, type) => response[type].reason;
+const reasonInconclusive = (response, type) => `*Majority:* ${response[type].reason}
+
+  *Dissenting:* ${response[type].dissenting}`;
+const reasonSection = (response, type) => response[type].conclusive ? reasonConclusive(response, type) : reasonInconclusive(response, type);
+
+// Template for the summary line at the end of a PR review.
+const summaryLine = (respose) => {
+  if (response.vagueness.concerning && response.vagueness.conclusive) {
+    return "Code consistency could not be evaluated because the commit message was too vague. A review of the individual commits will be conducted.";
+  }
+
+  if (response.contradicting.concerning || response.incomplete.concerning) {
+    return "The pull request should be reviewed carefully for the reasons identified above.";
+  }
+
+  return "The pull request passes all code consistency checks. The changes should still be reviewed for desirability.";
+}
 
 
 // Template: pull request review, positive for vagueness
 const vagueReport = (response) => `# [Vulcan](${vulcan_project_url}) Pull Request Review
 
-1. **Vagueness:** ${response.vagueness.concerning ? "concerning" : "OK"}
+1. **Vagueness:** ${consistencyHeader(response, "vagueness")}
 
-   ${response.vagueness.reason}
+   ${reasonSection(response, "vagueness")}
 
-Code consistency could not be evaluated because the commit message was too vague. A review of the individual commits will be conducted.`;
+${summaryLine(response)}`;
 
 
 // Template: pull request review, negative for vagueness
 const nonvagueReport = (response) => `# [Vulcan](${vulcan_project_url}) Pull Request Review
 
-1. **Vagueness:** ${response.vagueness.concerning ? "concerning" : "OK"}
+1. **Vagueness:** ${consistencyHeader(response, "vagueness")}
 
-   ${response.vagueness.reason}
+   ${reasonSection(response, "vagueness")}
 
-2. **Contradicting:** ${response.contradicting.concerning ? "concerning" : "OK"}
+2. **Contradicting:** ${consistencyHeader(response, "contradicting")}
 
-   ${response.contradicting.reason}
+   ${reasonSection(response, "contradicting")}
 
-3. **Incomplete:** ${response.incomplete.concerning ? "concerning" : "OK"}
+3. **Incomplete:** ${consistencyHeader(response, "incomplete")}
 
-   ${response.incomplete.reason}
+   ${reasonSection(response, "incomplete")}
 
-${response.contradicting.concerning || response.incomplete.concerning ? "The pull request should be reviewed carefully for the reasons identified above." : "The pull request passes all code consistency checks. The changes should still be reviewed for desirability."}`;
+${summaryLine(response)}`;
 
 
 // Template: pull request review, error parsing model output
@@ -45,9 +71,9 @@ const vagueCommitReport = (response, hash, url, message, index) => `
 ## ${index}. [${message.subject}](${url})
 ${message.body === "" ? "*Commit message has no further details*\n\n" : "<blockquote>" + message.body + "</blockquote>"}
 
-1. **Vagueness:** ${response.vagueness.concerning ? "concerning" : "OK"}
+1. **Vagueness:** ${consistencyHeader(response, "vagueness")}
 
-   ${response.vagueness.reason}`;
+   ${reasonSection(response, "vagueness")}`
 
 
 // Template: commit review, negative for vagueness
@@ -55,17 +81,17 @@ const nonvagueCommitReport = (response, hash, url, message, index) => `
 ## ${index}. [${message.subject}](${url})
 ${message.body === "" ? "*Commit message has no further details*\n\n" : "<blockquote>" + message.body + "</blockquote>"}
 
-1. **Vagueness:** ${response.vagueness.concerning ? "concerning" : "OK"}
+1. **Vagueness:** ${consistencyHeader(response, "vagueness")}
 
-   ${response.vagueness.reason}
+   ${reasonSection(response, "vagueness")}
 
-2. **Contradicting:** ${response.contradicting.concerning ? "concerning" : "OK"}
+2. **Contradicting:** ${consistencyHeader(response, "contradicting")}
 
-   ${response.contradicting.reason}
+   ${reasonSection(response, "contradicting")}
 
-3. **Incomplete:** ${response.incomplete.concerning ? "concerning" : "OK"}
+3. **Incomplete:** ${consistencyHeader(response, "incomplete")}
 
-   ${response.incomplete.reason}`;
+   ${reasonSection(response, "incomplete")}`;
 
 
 // Template: commit review, error parsing model output
@@ -100,9 +126,62 @@ function splitCommitMessage(message) {
   }
 }
 
+function countVotes(outputs, key) {
+  // Count the votes for concerning / not concerning.
+  const votes = {
+    true: 0,
+    false: 0,
+  };
+  for (var response of outputs) {
+    const value = response[key].concerning;
+    votes[value]++;
+  }
+
+  // Check which is the majority - break ties in favor of reporting a problem.
+  // Even though we run an odd number of tests, there could be a tie if one of
+  // them failed to parse.
+  const majority_decision = (votes[true] >= votes[false]);
+  const result = {
+    concerning: majority_decision,
+    result_string: majority_decision ? "concerning" : "OK",
+    votes: votes[majority_decision],
+    total_votes: votes[true] + votes[false],
+    conclusive: (votes[majority_decision] >= super_majority_threshold,
+    reason: null,
+    dissenting: null,
+  };
+  if (!result.conclusive) {
+    result.result_string = "inconclusive";
+  }
+
+  // Grab the first valid reason string for each outcome.
+  for (var response of outputs) {
+    if (response[key].concerning === majority_decision) {
+      result.reason = result.reason || response[key].reason;
+    } else {
+      result.dissenting = result.dissenting || response[key].reason;
+    }
+  }
+
+  return result;
+}
+
+function mergeOutputs(outputs) {
+  const result = {};
+
+  result.vagueness = countVotes(outputs, "vagueness");
+  if (result.vagueness.concerning && result.vagueness.conclusive) {
+    result.contradicting = {};
+    result.incomplete = {};
+  } else {
+    result.contradicting = countVotes(outputs, "contradicting");
+    result.incomplete = countVotes(outputs, "incomplete");
+  }
+
+  return result;
+}
 
 export async function postPullRequestReview(github, context, core) {
-  const geminiOutput = process.env.GEMINI_OUTPUT;
   const issueNumber = context.payload.pull_request
     ? context.payload.pull_request.number
     : context.payload.issue.number;
@@ -112,20 +191,24 @@ export async function postPullRequestReview(github, context, core) {
     return;
   }
 
-  var comment;
-  try {
-    const response = parseModelOutput(geminiOutput);
-    if (response.vagueness.concerning) {
-      comment = vagueReport(response);
-    } else {
-      comment = nonvagueReport(response);
+  const outputs = [1, 2, 3, 4, 5].map(i => {
+    const output = fs.readFileSync(`vulcan-pr-${i}.txt`, 'utf8');
+    try {
+      return parseModelOutput(output);
+    } catch (error) {
+      console.warn(error);
     }
-    core.setOutput("should-review-commits", response.vagueness.concerning);
-  } catch (error) {
-    console.warn(error);
-    comment = parseErrorReport(geminiOutput);
-    core.setOutput("should-review-commits", true);
+  );
+  const result = mergeOutputs(outputs);
+
+  var comment;
+  const conclusively_vague = (result.vagueness.concerning && result.vagueness.conclusive);
+  if (conclusively_vague) {
+    comment = vagueReport(result);
+  } else {
+    comment = nonvagueReport(result);
   }
+  core.setOutput("should-review-commits", conclusively_vague);
 
   await github.rest.issues.createComment({
     owner: context.repo.owner,
@@ -154,37 +237,27 @@ export async function postAggregateCommitReview(github, context, core) {
 
   var index = 1;
   for (const [hash, commit] of Object.entries(commits)) {
-    const output = fs.readFileSync(`vulcan-review-${hash}.txt`, 'utf8');
-
-    var vagueness = "-";
-    var contradicting = "-";
-    var incomplete = "-";
+    const outputs = [1, 2, 3, 4, 5].map(i => {
+      const output = fs.readFileSync(`vulcan-commit-${hash}-${i}.txt`, 'utf8');
+      try {
+        return parseModelOutput(output);
+      } catch (error) {
+        console.warn(error);
+      }
+    );
+    const result = mergeOutputs(outputs);
 
     const url = `https://github.com/${context.repo.owner}/${context.repo.repo}/pull/${issueNumber}/changes/${hash}`
-
     const message = splitCommitMessage(commit.commit.message);
 
-    try {
-      const response = parseModelOutput(output);
-      vagueness = response.vagueness.concerning ? "concerning" : "OK";
-      if (response.vagueness.concerning) {
-        vagueness = "concerning";
-
-        details += vagueCommitReport(response, hash, url, message, index);
-      } else {
-        vagueness = "OK";
-        contradicting = response.contradicting.concerning ? "concerning" : "OK";
-        incomplete = response.incomplete.concerning ? "concerning" : "OK";
-
-        details += nonvagueCommitReport(response, hash, url, message, index);
-      }
-    } catch (error) {
-      console.warn(error);
-
-      details += parseErrorReportCommit(response, hash, url, message, index);
+    const conclusively_vague = (result.vagueness.concerning && result.vagueness.conclusive);
+    if (conclusively_vague) {
+      details += vagueCommitReport(response, hash, url, message, index);
+    } else {
+      details += nonvagueCommitReport(response, hash, url, message, index);
     }
 
-    table += `| ${index} | ${message.subject} [${hash}](${url}) | ${vagueness} | ${contradicting} | ${incomplete} |\n`;
+    table += `| ${index} | ${message.subject} [${hash}](${url}) | ${result.vagueness.result_string} | ${result.contradicting.result_string} | ${result.incomplete.result_string} |\n`;
     index++;
   }
 
